cc039a51522aee646e8d6856c9a5460061801286
Dmitry Kann <yktooo@gmail.com>
2016-03-17 21:51:18 +0100

    Make server bind to all interfaces

--- homepi.py
+++ homepi.py
@@ -66,7 +66,7 @@ def run():
 
     # Server settings
     # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-    server_address = ('127.0.0.1', 8000)
+    server_address = ('', 8000)
     httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
     print('running server...')
     httpd.serve_forever()
