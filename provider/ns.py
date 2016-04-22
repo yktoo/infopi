82a257eff065957d86a9a67d24d0d85c32263a33
Dmitry Kann <yktooo@gmail.com>
2016-04-22 14:01:26 +0200

    Fixed Changed status

--- provider/ns.py
+++ provider/ns.py
@@ -13,7 +13,7 @@ STATUS_MAP = {
     'NIET-MOGELIJK': 'impossible',  # Impossible
     'GEANNULEERD':   'canceled',    # Canceled
     'VERTRAAGD':     'delayed',     # Delayed
-    'PLAN-GEWIJZGD': 'changed',     # Changed
+    'GEWIJZIGD':     'changed',     # Changed
     'NIEUW':         'new',         # New
 }
 
