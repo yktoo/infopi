da308ccf641438c21f00d31a1dd1609031455733
Dmitry Kann <yktooo@gmail.com>
2016-04-22 12:14:46 +0200

    Restyling; added moon phases

--- provider/buienradar.py
+++ provider/buienradar.py
@@ -1,4 +1,6 @@
+from datetime import date, datetime
 from xml.etree import ElementTree
+from astral import Astral
 
 from .http import HttpDataProvider
 
@@ -55,6 +57,37 @@ ICON_TO_WI_CLASS_MAP = {
     'zz': 'wi-snow-wind',
 }
 
+MOON_PHASE_MAP = [
+    {'text': 'New',             'wicls': 'wi-moon-new'},
+    {'text': 'Waxing crescent', 'wicls': 'wi-moon-waxing-crescent-1'},
+    {'text': 'Waxing crescent', 'wicls': 'wi-moon-waxing-crescent-2'},
+    {'text': 'Waxing crescent', 'wicls': 'wi-moon-waxing-crescent-3'},
+    {'text': 'Waxing crescent', 'wicls': 'wi-moon-waxing-crescent-4'},
+    {'text': 'Waxing crescent', 'wicls': 'wi-moon-waxing-crescent-5'},
+    {'text': 'Waxing crescent', 'wicls': 'wi-moon-waxing-crescent-6'},
+    {'text': 'First quarter',   'wicls': 'wi-moon-first-quarter'},
+    {'text': 'Waxing gibbous',  'wicls': 'wi-moon-waxing-gibbous-1'},
+    {'text': 'Waxing gibbous',  'wicls': 'wi-moon-waxing-gibbous-2'},
+    {'text': 'Waxing gibbous',  'wicls': 'wi-moon-waxing-gibbous-3'},
+    {'text': 'Waxing gibbous',  'wicls': 'wi-moon-waxing-gibbous-4'},
+    {'text': 'Waxing gibbous',  'wicls': 'wi-moon-waxing-gibbous-5'},
+    {'text': 'Waxing gibbous',  'wicls': 'wi-moon-waxing-gibbous-6'},
+    {'text': 'Full',            'wicls': 'wi-moon-full'},
+    {'text': 'Waning gibbous',  'wicls': 'wi-moon-waning-gibbous-1'},
+    {'text': 'Waning gibbous',  'wicls': 'wi-moon-waning-gibbous-2'},
+    {'text': 'Waning gibbous',  'wicls': 'wi-moon-waning-gibbous-3'},
+    {'text': 'Waning gibbous',  'wicls': 'wi-moon-waning-gibbous-4'},
+    {'text': 'Waning gibbous',  'wicls': 'wi-moon-waning-gibbous-5'},
+    {'text': 'Waning gibbous',  'wicls': 'wi-moon-waning-gibbous-6'},
+    {'text': 'Third quarter',   'wicls': 'wi-moon-third-quarter'},
+    {'text': 'Waningcrescent',  'wicls': 'wi-moon-waning-crescent-1'},
+    {'text': 'Waningcrescent',  'wicls': 'wi-moon-waning-crescent-2'},
+    {'text': 'Waningcrescent',  'wicls': 'wi-moon-waning-crescent-3'},
+    {'text': 'Waningcrescent',  'wicls': 'wi-moon-waning-crescent-4'},
+    {'text': 'Waningcrescent',  'wicls': 'wi-moon-waning-crescent-5'},
+    {'text': 'Waningcrescent',  'wicls': 'wi-moon-waning-crescent-6'},
+]
+
 # Mapping of Dutch to English names for days of the week
 DOW_MAP = {
     'ma': 'Mon',
@@ -76,17 +109,6 @@ class BuienRadarDataProvider(HttpDataProvider):
         """
         self.station = station
 
-    @staticmethod
-    def get_element(parent, path: str):
-        """Finds and returns the first element by given XPath, or raises an exception if not found.
-        :param parent: Parent element
-        :param path: Tag name or path expression
-        """
-        el = parent.find(path)
-        if el is None:
-            raise LookupError('Element {} doesn\'t have a child matching path "{}"'.format(parent.tag, path))
-        return el
-
     def get_url(self):
         return 'http://xml.buienradar.nl/'
 
@@ -98,12 +120,12 @@ class BuienRadarDataProvider(HttpDataProvider):
         if e_root.tag != 'buienradarnl':
             raise LookupError('Root XML node is not <buienradarnl>')
 
-        e_station = self.get_element(
+        e_station = get_element(
             e_root,
             "./weergegevens/actueel_weer/weerstations/weerstation[@id='{}']".format(self.station))
 
         # Process the current weather
-        e_icon = self.get_element(e_station, 'icoonactueel')
+        e_icon = get_element(e_station, 'icoonactueel')
         current = {
             'station': {
                 'code':       e_station.findtext('stationcode'),
@@ -111,62 +133,111 @@ class BuienRadarDataProvider(HttpDataProvider):
                 'latitude':   e_station.findtext('lat'),
                 'longitude':  e_station.findtext('lon'),
             },
-            'temperature':    e_station.findtext('temperatuurGC'),       # In °C
+            'temperature':    e_station.findtext('temperatuurGC'),     # In °C
             'humidity':       e_station.findtext('luchtvochtigheid'),
-            'pressure':       e_station.findtext('luchtdruk'),           # In hPa
+            'pressure':       e_station.findtext('luchtdruk'),         # In hPa
             'wind': {
-                'dirText':    e_station.findtext('windrichting'),        # Text, like 'WZW'
-                'dirDegrees': e_station.findtext('windrichtingGR'),      # In degrees
-                'speed':      e_station.findtext('windsnelheidBF'),      # In bft
-                'gusts':      e_station.findtext('windstotenMS')         # In m/s
+                'dirText':    e_station.findtext('windrichting'),      # Text, like 'WZW'
+                'dirDegrees': e_station.findtext('windrichtingGR'),    # In degrees
+                'speed':      e_station.findtext('windsnelheidBF'),    # In bft
+                'gusts':      e_station.findtext('windstotenMS')       # In m/s
             },
-            'rain':           e_station.findtext('regenMMPU'),           # In mm/h
-            'visibility':     e_station.findtext('zichtmeters'),         # In metres
-            'iconUrl':        e_icon.text,                               # Full URL, ie. http://...
-            'iconWiClass':    self.get_wi_icon_class(e_icon.get('ID')),  # One of the wi-* classes
+            'rain':           e_station.findtext('regenMMPU'),         # In mm/h
+            'visibility':     e_station.findtext('zichtmeters'),       # In metres
+            'iconUrl':        e_icon.text,                             # Full URL, ie. http://...
+            'iconText':       e_icon.get('zin'),                       # In Dutch
+            'iconWiClass':    get_wi_icon_class(e_icon.get('ID')),     # One of the wi-* classes
             'text':           e_root.findtext('./weergegevens/verwachting_vandaag/samenvatting'),
         }
 
         # Process the forecast
         forecast = []
         for e_day in e_root.findall('./weergegevens/verwachting_meerdaags/*[datum]'):
-            e_day_icon = self.get_element(e_day, 'icoon')
+            e_day_icon = get_element(e_day, 'icoon')
             forecast.append({
-                'date':            e_day.findtext('datum'),                       # Full date, eg 'zondag 17 april 2016'
-                'dow':             DOW_MAP[e_day.findtext('dagweek')],            # Weekday name in English
-                'probSun':         e_day.findtext('kanszon'),                     # Probability in percent
-                'probSnow':        e_day.findtext('sneeuwcms'),                   # Probability in percent
+                'date':            e_day.findtext('datum'),                  # Full date, eg 'zondag 17 april 2016'
+                'dow':             DOW_MAP[e_day.findtext('dagweek')],       # Weekday name in English
+                'probSun':         e_day.findtext('kanszon'),                # Probability in percent
+                'probSnow':        e_day.findtext('sneeuwcms'),              # Probability in percent
                 'rain': {
-                    'probability': e_day.findtext('kansregen'),                   # Probability in percent
-                    'minAmount':   e_day.findtext('minmmregen'),                  # Minimum amount in mm
-                    'maxAmount':   e_day.findtext('maxmmregen'),                  # Maximum amount in mm
+                    'probability': e_day.findtext('kansregen'),              # Probability in percent
+                    'minAmount':   e_day.findtext('minmmregen'),             # Minimum amount in mm
+                    'maxAmount':   e_day.findtext('maxmmregen'),             # Maximum amount in mm
                 },
                 'temperature': {
-                    'lowMin':      e_day.findtext('mintemp'),                     # In °C
-                    'lowMax':      e_day.findtext('mintempmax'),                  # In °C
-                    'highMin':     e_day.findtext('maxtemp'),                     # In °C
-                    'highMax':     e_day.findtext('maxtempmax'),                  # In °C
+                    'lowMin':      e_day.findtext('mintemp'),                # In °C
+                    'lowMax':      e_day.findtext('mintempmax'),             # In °C
+                    'highMin':     e_day.findtext('maxtemp'),                # In °C
+                    'highMax':     e_day.findtext('maxtempmax'),             # In °C
                 },
                 'wind': {
-                    'dirText':     e_day.findtext('windrichting'),                # Text, like 'WZW'
-                    'speed':       e_day.findtext('windkracht'),                  # In bft
+                    'dirText':     e_day.findtext('windrichting'),           # Text, like 'WZW'
+                    'speed':       e_day.findtext('windkracht'),             # In bft
                 },
-                'iconUrl':         e_day_icon.text,                               # Full URL, ie. http://...
-                'iconWiClass':     self.get_wi_icon_class(e_day_icon.get('ID')),  # One of the wi-* classes
+                'iconUrl':         e_day_icon.text,                          # Full URL, ie. http://...
+                'iconWiClass':     get_wi_icon_class(e_day_icon.get('ID')),  # One of the wi-* classes
             })
 
-        return {
-            'updated':  e_station.findtext('datum'),
-            'current':  current,
-            'forecast': forecast
+        # Process sunrise/sunset
+        e_buienradar = get_element(e_root, './weergegevens/actueel_weer/buienradar')
+        moon_phase = Astral().moon_phase(date.today())
+        sunmoon = {
+            'sunrise':          iso_datetime(e_buienradar.findtext('zonopkomst')),  # ISO8601 sunrise time
+            'sunset':           iso_datetime(e_buienradar.findtext('zononder')),    # ISO8601 sunset time
+            'moonPhase':        moon_phase,                                         # Moon phase in days (0..27)
+            'moonPhaseText':    get_moon_text(moon_phase),                          # Textual description
+            'moonPhaseWiClass': get_moon_wi_icon_class(moon_phase),                 # One of the wi-* classes
         }
 
-    @staticmethod
-    def get_wi_icon_class(icon_id: str):
-        """Return name of the weather-icon class for the given BuienRadar icon URL.
-        :param icon_id: one- or two-letter icon ID
-        """
-        if icon_id in ICON_TO_WI_CLASS_MAP:
-            return ICON_TO_WI_CLASS_MAP[icon_id]
-        else:
-            return 'wi-na'
+        return {
+            'updated':  iso_datetime(e_station.findtext('datum')),
+            'current':  current,
+            'forecast': forecast,
+            'sunMoon':  sunmoon
+        }
+
+
+def get_element(parent, path: str):
+    """Finds and returns the first element by given XPath, or raises an exception if not found.
+    :param parent: Parent element
+    :param path: Tag name or path expression
+    """
+    el = parent.find(path)
+    if el is None:
+        raise LookupError('Element {} doesn\'t have a child matching path "{}"'.format(parent.tag, path))
+    return el
+
+
+def get_wi_icon_class(icon_id: str):
+    """Return name of the weather-icon class for the given BuienRadar icon URL.
+    :param icon_id: one- or two-letter icon ID
+    :return: wi-* class name
+    """
+    if icon_id in ICON_TO_WI_CLASS_MAP:
+        return ICON_TO_WI_CLASS_MAP[icon_id]
+    else:
+        return 'wi-na'
+
+
+def get_moon_text(moon_phase: int) -> str:
+    """Return textual name of a moon phase.
+    :param moon_phase: moon phase in days (0..27)
+    :return: Phase name
+    """
+    return MOON_PHASE_MAP[moon_phase]['text'] if 0 <= moon_phase <= 27 else ''
+
+
+def get_moon_wi_icon_class(moon_phase: int) -> str:
+    """Return name of the weather-icon class corresponding to a moon phase.
+    :param moon_phase: moon phase in days (0..27)
+    :return: wi-* class name or empty string if the phase is incorrect
+    """
+    return MOON_PHASE_MAP[moon_phase]['wicls'] if 0 <= moon_phase <= 27 else ''
+
+
+def iso_datetime(dt: str) -> str:
+    """Convert BuienRadar's datetime representation into ISO8601.
+    :param dt: date and time in BuienRadar's format
+    :return: date and time string in the ISO8601 format
+    """
+    return datetime.strptime(dt, '%m/%d/%Y %H:%M:%S').isoformat()
