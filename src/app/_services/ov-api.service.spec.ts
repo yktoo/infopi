9e9eee7f9fdf29875aa9b7def2f68fa19aee4fcf
Dmitry Kann <yktooo@gmail.com>
2020-04-04 18:32:53 +0200

    Add unit tests for OvApiService

--- src/app/_services/ov-api.service.spec.ts
+++ src/app/_services/ov-api.service.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { OvApiService } from './ov-api.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('OvApiService', () => {
+
     let service: OvApiService;
+    let httpTestingController: HttpTestingController;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -12,10 +14,36 @@ describe('OvApiService', () => {
                 HttpClientTestingModule,
             ],
         });
+
+        // Inject the HTTP test controller for each test
+        httpTestingController = TestBed.inject(HttpTestingController);
+
+        // Instantiate service under test
         service = TestBed.inject(OvApiService);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
+
+    it('#getDepartureTimes() should request and unwrap departure times', () => {
+        service.getDepartureTimes('zazzoo').subscribe(data => expect(data).toEqual({foo: 'bar'}));
+
+        // Mock the HTTP service
+        const req = httpTestingController.expectOne('http://v0.ovapi.nl/stopareacode/zazzoo');
+
+        // Verify request method
+        expect(req.request.method).toEqual('GET');
+
+        // Respond with test data
+        req.flush({
+            zazzoo: {
+                foo: 'bar',
+            }
+        });
+
+        // Verify there's no outstanding request
+        httpTestingController.verify();
+    });
+
 });
