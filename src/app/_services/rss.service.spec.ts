55e71ab664f4f2dfae4ff56ff9966a992aaa9ad3
Dmitry Kann <yktooo@gmail.com>
2020-04-09 19:20:40 +0200

    Add unit tests for RssService, ClockComponent

--- src/app/_services/rss.service.spec.ts
+++ src/app/_services/rss.service.spec.ts
@@ -1,21 +1,144 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RssService } from './rss.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ConfigService } from './config.service';
 
 describe('RssService', () => {
+
     let service: RssService;
+    let httpTestingController: HttpTestingController;
+
+    /**
+     * Mock ConfigService class that returns a fixed CORS proxy config string.
+     */
+    class MockConfigService {
+        get corsProxy() {
+            return 'PROXY:';
+        }
+    }
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [
                 HttpClientTestingModule,
             ],
+            providers: [
+                { provide: ConfigService, useClass: MockConfigService },
+            ],
         });
+
+        // Inject the HTTP test controller for each test
+        httpTestingController = TestBed.inject(HttpTestingController);
+
+        // Instantiate service under test
         service = TestBed.inject(RssService);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
+
+    it('#getRssItems() should request and unwrap RSS feed', () => {
+        const url = 'http://super-rss-feed.com/rss.xml';
+        service.getRssItems(url)
+            .subscribe(data => {
+                expect(data.title[0]).toEqual('Superfeed');
+                expect(data.item).toEqual([
+                    {
+                        title: ['Super post'],
+                        link: ['http://the-link.com'],
+                        description: ['This is the best post'],
+                    },
+                    {
+                        title: ['Next post'],
+                        link: ['http://the-link-2.com'],
+                        description: ['This is the next best post'],
+                    },
+                ]);
+            });
+
+        // Mock the HTTP service
+        const req = httpTestingController.expectOne('PROXY:' + url);
+
+        // Verify request method
+        expect(req.request.method).toEqual('GET');
+
+        // Respond with test data
+        req.flush(
+            '<?xml version="1.0" encoding="utf-8" ?>' +
+            '<rss version="2.0">' +
+                '<channel>' +
+                    '<title>Superfeed</title>' +
+                    '<item>' +
+                        '<title>Super post</title>' +
+                        '<link>http://the-link.com</link>' +
+                        '<description>This is the best post</description>' +
+                    '</item>' +
+                    '<item>' +
+                        '<title>Next post</title>' +
+                        '<link>http://the-link-2.com</link>' +
+                        '<description>This is the next best post</description>' +
+                    '</item>' +
+                '</channel>' +
+            '</rss>');
+
+        // Verify there's no outstanding request
+        httpTestingController.verify();
+    });
+
+    it('#getRssItems() should request and unwrap Atom feed', () => {
+        const url = 'http://super-atom-feed.com/atom.xml';
+        service.getRssItems(url)
+            .subscribe(data => {
+                expect(data.title[0]).toEqual('Atomfeed');
+                expect(data.entry).toEqual([
+                    {
+                        title: ['Super Atom Post'],
+                        link: [{
+                            $: {href: 'http://atom-link-03'}
+                        }],
+                        id: ['C0FFEE'],
+                        summary: ['Summary text'],
+                    },
+                    {
+                        title: ['Mega Atom Post'],
+                        link: [{
+                            $: {href: 'http://atom-link-33'}
+                        }],
+                        id: ['aBBa'],
+                        summary: ['Lots of info'],
+                    },
+                ]);
+            });
+
+        // Mock the HTTP service
+        const req = httpTestingController.expectOne('PROXY:' + url);
+
+        // Verify request method
+        expect(req.request.method).toEqual('GET');
+
+        // Respond with test data
+        req.flush(
+            '<?xml version="1.0" encoding="utf-8"?>' +
+            '<feed xmlns="http://www.w3.org/2005/Atom">' +
+                '<title>Atomfeed</title>' +
+                '<entry>' +
+                    '<title>Super Atom Post</title>' +
+                    '<link href="http://atom-link-03"/>' +
+                    '<id>C0FFEE</id>' +
+                    '<summary>Summary text</summary>' +
+                '</entry>' +
+                '<entry>' +
+                    '<title>Mega Atom Post</title>' +
+                    '<link href="http://atom-link-33"/>' +
+                    '<id>aBBa</id>' +
+                    '<summary>Lots of info</summary>' +
+                '</entry>' +
+            '</feed>');
+
+        // Verify there's no outstanding request
+        httpTestingController.verify();
+    });
+
 });
--- src/app/clock/clock.component.spec.ts
+++ src/app/clock/clock.component.spec.ts
@@ -14,6 +14,7 @@ describe('ClockComponent', () => {
     }));
 
     beforeEach(() => {
+        jasmine.clock().mockDate(new Date('2006-05-04 23:01:59'));
         fixture = TestBed.createComponent(ClockComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
@@ -22,4 +23,17 @@ describe('ClockComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should display date', () => {
+        const element: HTMLElement = fixture.nativeElement;
+        expect(element.querySelector('.date').textContent).toEqual('4.05');
+        expect(element.querySelector('.day-of-week').textContent).toEqual('Thursday');
+    });
+
+    it('should display time', () => {
+        const element: HTMLElement = fixture.nativeElement;
+        expect(element.querySelector('.hhmm').textContent).toEqual('23:01');
+        expect(element.querySelector('.ss').textContent).toEqual(':59');
+    });
+
 });
